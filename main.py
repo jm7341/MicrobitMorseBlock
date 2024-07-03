1191903a9d45726d48ac7a9cbd8a4527804c02fe
jm7341 <122776044+jm7341@users.noreply.github.com>
2024-07-03 22:04:47 +0900

    dz

--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 def on_received_number(receivedNumber):
-    pass
+    if receivedNumber == 1:
+        basic.clear_screen()
+        basic.pause(5)
+        basic.clear_screen()
+        basic.show_leds("""
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            """)
+        basic.pause(10)
+        basic.clear_screen()
+    if receivedNumber == 3:
+        basic.clear_screen()
+        basic.pause(5)
+        basic.clear_screen()
+        basic.show_leds("""
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            """)
+        basic.pause(100)
+        basic.clear_screen()
 radio.on_received_number(on_received_number)
 
 def on_button_pressed_a():
     global a
     a = 1
     radio.send_number(a)
-    radio.send_number(0)
+    basic.pause(5)
     a = 0
+    radio.send_number(0)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global a
     a = 3
     radio.send_number(a)
-    radio.send_number(0)
+    basic.pause(5)
     a = 0
+    radio.send_number(0)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 a = 0
 a = 0
 radio.set_group(77)
-
-def on_forever():
-    if a == 1:
-        basic.show_leds("""
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            """)
-        basic.pause(200)
-        basic.clear_screen()
-    if a == 3:
-        basic.show_leds("""
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            """)
-        basic.pause(600)
-        basic.clear_screen()
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,43 +1,58 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    if (b == 1) {
+        music.play(music.stringPlayable("C E G B C5 B - - ", 500), music.PlaybackMode.UntilDone)
+    }
     if (receivedNumber == 1) {
         basic.clearScreen()
-        basic.pause(60)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.pause(200)
+        basic.pause(5)
         basic.clearScreen()
+        basic.showLeds(`
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            `)
+        basic.pause(5)
+        basic.clearScreen()
+        b += 1
+        basic.pause(10)
+        b += 1
     }
     if (receivedNumber == 3) {
         basic.clearScreen()
-        basic.pause(60)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.pause(600)
+        basic.pause(5)
         basic.clearScreen()
+        basic.showLeds(`
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # .
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+        b += 1
+        basic.pause(10)
+        b += 1
     }
 })
 input.onButtonPressed(Button.A, function () {
     a = 1
     radio.sendNumber(a)
-    radio.sendNumber(0)
+    basic.pause(5)
     a = 0
+    radio.sendNumber(0)
 })
 input.onButtonPressed(Button.B, function () {
     a = 3
     radio.sendNumber(a)
-    radio.sendNumber(0)
+    basic.pause(5)
     a = 0
+    radio.sendNumber(0)
 })
 let a = 0
+let b = 0
+b = 0
 a = 0
 radio.setGroup(77)
